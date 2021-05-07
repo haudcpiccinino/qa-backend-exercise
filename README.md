1b952616e6654f27f5237dcb9905850ef51a286e
Clyde Piccinino <clyde.piccinino@haud.com>
2021-05-07 16:57:22 +0200

    add user create remove list search

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # QA Backend Exercise
+
+
+Add user to list
+```bash
+curl -X POST --header "Content-Type: application/json" -d '{"name":"My name"}' http://localhost:8000/user
+```
+
+Remove user from list
+
+```bash
+curl -X DELETE --header "Content-Type: application/json" -d '{"name":"My name"}' http://localhost:8000/user
+```
+
+---
+
+[Link](https://qa-backend-exercise.herokuapp.com)
--- exercise.py
+++ exercise.py
@@ -1,12 +1,51 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
+
+users = [
+    dict (
+        name = "Dorothy Gale"
+    )
+]
 
 @app.route('/info', methods=['GET'])
 def respond():
     response = {}
     response["version"] = 1
-    return jsonify(response)
+    return jsonify(response), 200
+
+@app.route('/user', methods=['GET'])
+def user_list():
+    if 'name' in request.args:
+        n = request.args.get('name')
+        found = []
+        for user in users:
+            if n.lower() in user.get('name').lower():
+                found.append(user)
+        return jsonify(found), 200
+    else: 
+        return jsonify(users), 200
+
+@app.route('/user', methods=['POST'])
+def user_create():
+    req = request.get_json()
+    user_name = req.get('name')
+    user = dict(
+        name = user_name
+    )
+    users.append(user)
+    return jsonify({'message':'Added '+user_name}), 201
+
+@app.route('/user', methods=['DELETE'])
+def user_delete():
+    req = request.get_json()
+    user_name = req.get('name')
+    user = dict(
+        name = user_name
+    )
+    users.remove(user)
+    return jsonify({'message':'Deleted '+user_name}), 200
 
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server</h1>"
+    return render_template('welcome.html')
+
